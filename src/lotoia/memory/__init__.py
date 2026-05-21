f09f478a965ec6b05bb3f843cbe22d911d367de2
lotoia-analytics <lotoia2026@gmail.com>
2026-05-21 20:29:06 -0300

    add institutional state diff engine

--- src/lotoia/memory/__init__.py
+++ src/lotoia/memory/__init__.py
@@ -6,6 +6,7 @@ from .memory_registry import (
     MemoryState,
 )
 from .memory_evolution import MemoryEvolution, MemoryEvolutionStep, build_adaptive_evolution_tracking
+from .memory_diff import InstitutionalStateDiff, MemoryDiffAxis, build_institutional_state_diff
 from .memory_timeline import MemoryTimeline, MemoryTimelineEntry, build_memory_timeline
 
 __all__ = [
@@ -17,6 +18,9 @@ __all__ = [
     "MemoryEvolution",
     "MemoryEvolutionStep",
     "build_adaptive_evolution_tracking",
+    "InstitutionalStateDiff",
+    "MemoryDiffAxis",
+    "build_institutional_state_diff",
     "MemoryTimeline",
     "MemoryTimelineEntry",
     "build_memory_timeline",
